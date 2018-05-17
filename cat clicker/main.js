4d9809e23d871743b65871ba31d036bb79e14625
SD13 <SD13@mitt.ca>
2018-05-17 11:23:53 -0500

    First Requirements Change

--- cat clicker/main.js
+++ cat clicker/main.js
@@ -1,10 +1,20 @@
-const count = document.querySelector('#count');
-const image = document.querySelector('.cat');
+const count1 = document.querySelector('#count1');
+const image1 = document.querySelector('.img1');
+const count2 = document.querySelector('#count2');
+const image2 = document.querySelector('.img2');
 
+document.querySelector('.cat1n').textContent = "ABC";
+document.querySelector('.cat2n').textContent = "XYZ";
 
-image.onclick = function(){
-    count.textContent++;
+image1.onclick = function(){
+    count1.textContent++;
 }
-document.querySelector('.reset').onclick = function(){
-    count.textContent = 0;
+image2.onclick = function(){
+    count2.textContent++;
+}
+document.querySelector('.reset1').onclick = function(){
+    count1.textContent = 0;
+}
+document.querySelector('.reset2').onclick = function(){
+    count2.textContent = 0;
 }
